cd6b0e14fdaad0f1876831e98bc035538a5c2a21
kevindecruz <kevin.decruz@crossknowledge.com>
2018-12-10 14:33:00 +0100

    Add classes CkStreamedResponse and CkStreamReader

---
+++ CkStreamReader.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevindecruz
+ * Date: 10/12/18
+ * Time: 14:27
+ */
+
+namespace CrossKnowledge\Stream\CkStreamReader;
+
+
+class CkStreamReader
+{
+    /**
+     * 2097152 bytes = 2 Mb
+     */
+    const FREAD_LENGTH = 2097152;
+
+    /**
+     * Specific expected header
+     */
+    const HEADER_CONTENT_COUNT = 'Content-Count';
+
+    /**
+     * Unactivity max delay before closing the connection
+     */
+    const UNACTIVITY_DELAY = 60;
+
+    private $uri = '';
+
+    private $client = null;
+
+    private $callback;
+
+    private $maxDelay;
+
+    private $contentCountTotal = 0;
+
+    private $contentCount = 0;
+
+    private $reliquat = '';
+
+    public function __construct($uri, $callback = null, $maxDelay = self::UNACTIVITY_DELAY)
+    {
+        $this->uri = $uri;
+        $this->callback = $callback;
+        $this->maxDelay = $maxDelay;
+        $this->client = new \GuzzleHttp\Client();
+    }
+
+    public function getResponse()
+    {
+        $response = $this->client->request('GET', $this->uri,
+            [
+                'stream' => true,
+                'sink' => fopen('php://stdout', 'w')
+            ]);
+        $stream = $response->getBody();
+        $this->contentCountTotal = $response->getHeader(self::HEADER_CONTENT_COUNT)[0];
+        $cptLoop = 1;
+        while (!$stream->eof()) {
+            $lines = explode(PHP_EOL, $this->reliquat . $stream->read(self::FREAD_LENGTH));
+            $nbLines = count($lines);
+            if (substr($lines[$nbLines-1], -1, 1) == PHP_EOL) {
+                $this->reliquat = '';
+            } else {
+                $this->reliquat = $lines[$nbLines-1];
+                unset($lines[$nbLines-1]);
+            }
+
+            call_user_func($this->callback, $lines, $cptLoop);
+
+            $this->contentCount += count($lines);
+
+            if ($this->contentCount >= $this->contentCountTotal) {
+                $stream->close();
+                break;
+            }
+            $cptLoop ++;
+        }
+    }
+}
---
+++ CkStreamedResponse.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevindecruz
+ * Date: 07/12/18
+ */
+
+namespace CrossKnowledge\Stream\CkStreamedResponse;
+
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+class CkStreamedResponse extends StreamedResponse
+{
+    /**
+     * Nb lines trigger to flush output and send CSV lines in getLinkedContentAction WS
+     */
+    const STREAM_QUERY_LIMIT = 1000;
+
+    /**
+     * @var int batch limit
+     */
+    private $queryLimit = 0;
+
+    public function __construct($contentQuery, $status = 200, $headers = array(), $queryLimit = self::STREAM_QUERY_LIMIT)
+    {
+        parent::__construct(
+            function() use ($contentQuery) {
+                $this->calbackStream($contentQuery);
+            },
+            $status,
+            array_merge($headers, [CkStreamReader::HEADER_CONTENT_COUNT => $contentQuery->count()])
+        );
+        $this->queryLimit = $queryLimit;
+
+    }
+
+    private function calbackStream(\ModelCriteria $contentQuery)
+    {
+        //Do all queries in a transaction to avoid insert / update issue between the first and the last query
+        //otherwise contentCount should be false
+        \Propel::getConnection()->beginTransaction();
+        $contentQuery->setLimit($this->queryLimit);
+        $offSet = 0;
+        do {
+            $contents = $contentQuery
+                ->setOffset($offSet)
+                ->find();
+            foreach ($contents as $content) {
+                if (fputcsv(fopen('php://output', 'w'), $content) == false) {
+                    error_log('fputcsv failed on \'php://output\' with content : ' . serialize($content));
+                }
+            }
+            flush();
+            sleep(5);
+            $offSet += $this->queryLimit;
+        } while (count($contents) == $this->queryLimit);
+        //Nothing to commit but we need to close transaction
+        \Propel::getConnection()->commit();
+    }
+}
